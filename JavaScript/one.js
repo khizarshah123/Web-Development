ded0aa68f3fed8c40108451ee8fb8f6a79eebaef
Khizar Abbas <abbaskhizar471@gmail.com>
2026-05-27 23:56:09 +0500

    Add files via upload

---
+++ JavaScript/one.js
@@ -0,0 +1,92 @@
+
+<!-- Internel js  -->
+
+
+<script>
+
+        // alert("Khizr Here") // pre define prototype functions 
+
+        // document.write("Khizr Khan")
+
+        // console.log("Khizar Khan");
+        
+        // confirm("Are u Agree")
+
+
+        // variables => data store => re-use 
+
+        // variant data type => data type define not 
+
+
+        // var 
+        // let 
+        // const 
+
+        //     type variable_name = value
+                        
+            // 1!@#!#$!
+
+
+            // var a = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam ea assumenda placeat quod ad nostrum quisquam nisi facere quis officiis, ullam accusantium natus. Voluptate obcaecati animi eligendi sit nulla!';
+
+            // document.write(a)
+
+            // alert(a)
+
+            // Operators 
+            //     Arthmetic 
+            //         Calculation 
+            //         + 
+            //         -
+            //         *
+            //         /
+            //         %
+
+            //     Relational 
+            //     Assingment Operator 
+            //     Logical 
+            //     Increment / decrement Operator 
+            //     Ternery Operator 
+
+
+
+            var a = 10;
+            var b =350
+
+            // document.write("Addation is : ")
+            // document.write(a + 50)
+
+            document.write("Addation is : " , a+b)
+
+
+
+</script>
+
+
+Assingment 
+
+    a = 50
+    b = 10
+
+    Swapp 
+
+    a = 10 
+    b = 50
+
+
+    conversion 
+
+        min = 500 
+            Total Hours 
+            Total Seconds
+
+        Second = 5000 
+            Total Hours 
+            Total Min 
+
+
+        Hour = 5000 
+            Total Seconds 
+            Total Min 
+
+            
