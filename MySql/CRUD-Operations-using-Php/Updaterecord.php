e67c1decf1f6641e5d2ba84e0b07416ee5837a85
Khizar Abbas <abbaskhizar471@gmail.com>
2026-07-11 17:44:28 +0500

    Add files via upload

---
+++ MySql/CRUD-Operations-using-Php/Updaterecord.php
@@ -0,0 +1,30 @@
+ <?php
+
+$conn =  mysqli_connect('localhost','root','','crud operations') or die();
+
+if(isset($_POST['updatebtn']))
+    {
+    
+        $id = $_POST['id'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+            $query = "UPDATE data SET email ='$email' , password='$password' WHERE id ='$id' ";
+
+       $res = mysqli_query($conn,$query);
+
+
+   if($res)
+    {
+      header('location:Table.php');
+    }
+
+    else
+        {
+             echo('Record not Updated');
+        }
+
+    }
+    
+
+?> 
---
+++ MySql/CRUD-Operations-using-Php/deleterecord.php
@@ -0,0 +1,26 @@
+<?php
+$conn = mysqli_connect('localhost','root','','crud operations') or die();
+
+if(isset($_POST['deleteid']))
+    {
+
+  $deleteid = $_POST['deleteid'];
+
+ $query = "DELETE FROM data WHERE id = '$deleteid' ";
+
+   $res = mysqli_query($conn,$query) ;
+
+
+   if($res)
+    {
+      header('location:Table.php');
+    }
+
+    else
+        {
+             echo('Record ! Deleted');
+        }
+
+    }
+
+?>
